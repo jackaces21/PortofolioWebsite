a58977127ed0e74f10a0b8463ab9102f99cbde9b
jackaces21 <iam.ahmadsholeh@gmail.com>
2022-06-09 18:17:45 +0700

    commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+import Porto1 from "./img/porto1.png"
+import Porto2 from "./img/porto2.png"
+
+export const products = [
+    {
+        id: 1,
+        img: { Porto1 }
+
+    },
+    {
+        id: 2,
+        img: { Porto2 }
+    }
+]
