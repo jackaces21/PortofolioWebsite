5f79fc5cbc909185a0962d644716a86820b2b33c
amd123 <iam.ahmadsholeh@gmail.com>
2023-10-28 17:51:52 +0700

    update

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import Product from '../product/Product'
 import './productList.css'
 import { products } from '../../data'
-<<<<<<< HEAD
-=======
-import img1 from '../../img/porto1.png'
-import img2 from '../../img/porto2.png'
-import img3 from '../../img/sun.png'
+// import img1 from '../../img/porto1.png'
+// import img2 from '../../img/porto2.png'
+// import img3 from '../../img/sun.png'
 
->>>>>>> a58977127ed0e74f10a0b8463ab9102f99cbde9b
 
 
 
@@ -20,19 +17,9 @@ const ProductList = () => {
                 <p className="pl-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus fugiat facere voluptas illo. Vel quam unde fuga distinctio deserunt est mollitia modi, molestias asperiores, cum, aperiam dolorem maiores commodi ea.</p>
             </div>
             <div className="pl-list">
-<<<<<<< HEAD
                 {products.map(item => (
                     <Product key={item.id} img={item.img} />
                 ))}
-=======
-                <img src={img1} alt="" className='p' />
-                <img src={img2} alt="" className='p' />
-                <img src={img3} alt="" className='p' />
-
-                {/* {products.map(item => (
-                    <Product key={item.id} img={item.img} />
-                ))} */}
->>>>>>> a58977127ed0e74f10a0b8463ab9102f99cbde9b
             </div>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -1,30 +1,19 @@
 import Porto1 from "./img/porto1.png"
 import Porto2 from "./img/porto2.png"
-<<<<<<< HEAD
 import Sun from './img/sun.png'
-=======
->>>>>>> a58977127ed0e74f10a0b8463ab9102f99cbde9b
 
 export const products = [
-    {
-        id: 1,
-<<<<<<< HEAD
-        img: Porto1
-=======
-        img: { Porto1 }
->>>>>>> a58977127ed0e74f10a0b8463ab9102f99cbde9b
+        {
+                id: 1,
+                img: Porto1
 
-    },
-    {
-        id: 2,
-<<<<<<< HEAD
-        img: Porto2
-    },
-    {
-        id: 3,
-        img: Sun
-=======
-        img: { Porto2 }
->>>>>>> a58977127ed0e74f10a0b8463ab9102f99cbde9b
-    }
+        },
+        {
+                id: 2,
+                img: Porto2
+        },
+        {
+                id: 3,
+                img: Sun
+        }
 ]
